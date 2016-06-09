58c5db50b7b2809a625a8abc934e19af3a6de6bf
sacreman <sacreman@gmail.com>
2016-06-09 10:48:19 +0100

    better private pack errors

--- dlcli/cli/install.py
+++ dlcli/cli/install.py
@@ -18,9 +18,25 @@ def install():
 @click.argument('name')
 def template(name):
     try:
-        print templates_api.install_template(name=name, **context.settings)
+        click.confirm('This will uninstall and re-install the template as a pack. Are you sure?', abort=True)
+
+        resp = packs.delete_pack(name=name, **context.settings)
+        if resp.status_code == 204:
+            click.echo('Deleted pack ' + name)
+        else:
+            click.echo('Error deleting ' + name + '. Status Code: ' + click.style(
+                str(resp.status_code),
+                fg='red'))
+
+        resp = templates_api.install_template(name=name, **context.settings)
+        if resp.status_code == 200:
+            click.echo('Installed pack ' + name)
+        else:
+            click.echo('Error deleting ' + name + '. Status Code: ' + click.style(
+                str(resp.status_code),
+                fg='red'))
     except Exception, e:
-        print 'Create template failed. %s' % e
+        print 'Install template failed. %s' % e
         sys.exit(1)
 
 
